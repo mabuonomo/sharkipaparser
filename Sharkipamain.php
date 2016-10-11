9a82fbccb056717e35b6e478207da3b71fb10643
Mario Buonomo <ma.buonomo@gmail.com>
2016-10-11 09:51:03 +0200

    errori di compilazione

--- Sharkipamain.php
+++ Sharkipamain.php
@@ -13,23 +13,25 @@
  */
 class Sharkipamain {
 
-    private $path_ipa;// = '';
-    private $path_tmp = 'unzip/ipa/';
+    protected $path_ipa; // = '';
+    protected $path_tmp = 'unzip/ipa/';
 
     function load_ipa($path) {
         $path_ipa = $path;
 
         $zip = new ZipArchive;
-        
+
         //$path_tmp .= $path_ipa;
-        
-        if (!file_exists($path_tmp))
-                mkdir($path_tmp, 0777, true);
-        
-        if ($zip->open($path) === TRUE) {
-            $zip->extractTo($path);
+
+        if (!file_exists($this->path_tmp))
+            mkdir($this->path_tmp, 0777, true);
+
+        if ($zip->open($this->path_ipa) === TRUE) {
+            $zip->extractTo($this->path_tmp);
             $zip->close();
         }
     }
 
 }
+
+?>
