73db45a2e7cf47a2cc6a6eb05b6497e9a87c936f
Mario Buonomo <ma.buonomo@gmail.com>
2016-10-21 09:32:21 +0200

    codeclan

--- class/SharkIpaParser.php
+++ class/SharkIpaParser.php
@@ -2,13 +2,6 @@
 
 namespace SharkIpaParser;
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
-//load namespace from vendor by composer
-require __DIR__ . '/../vendor/autoload.php';
-
 use CFPropertyList;
 
 /**
--- test.php
+++ test.php
@@ -1,10 +1,17 @@
 <?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 
 session_start();
 
+require __DIR__ . '/vendor/autoload.php';
+
 require_once 'class/SharkIpaParser.php';
 
-$shark = new SharkIpaParser\SharkIpaParserHelper();
+//use SharkIpaParser as Shark;
+
+$shark = new \SharkIpaParser\SharkIpaParserHelper();
 
 $shark->load_ipa('C:\Users\mario\Desktop\test.ipa');
 echo $shark->read_plist()['CFBundleName'];
