384313cf9d45b4812d4b43369a2ec81c5611e701
mario <mario@mario-Aspire-one-1-431>
2016-10-23 17:22:44 +0200

    .

--- test.php
+++ test.php
@@ -7,11 +7,12 @@ session_start();
 
 require __DIR__ . '/vendor/autoload.php';
 
-require_once 'class/SharkIpaParser.php';
+// require_once 'class/SharkIpaParser.php';
+use sharkipaparser;
 
 //use SharkIpaParser as Shark;
 
-$shark = new \SharkIpaParser\SharkIpaParserHelper();
+$shark = new sharkipaparser\SharkIpaParserHelper();
 
 $shark->load_ipa('C:\Users\mario\Desktop\test.ipa');
 echo $shark->read_plist()['CFBundleName'];
